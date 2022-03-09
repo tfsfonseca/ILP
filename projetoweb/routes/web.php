c396afedda56c4d332ca4890f30cf27273fd4513
tfsfonseca <88214662+tfsfonseca@users.noreply.github.com>
2022-03-08 21:58:21 -0300

    Aula 08.03

--- projetoweb/routes/web.php
+++ projetoweb/routes/web.php
@@ -19,3 +19,23 @@ Route::get('/', [HomeController::class, 'welcome']);
 Route::get('/exemplo', [HomeController::class, 'exemplo']);
 
 Route::post('/resultado', [HomeController::class,'resultado']);
+
+Route::get('/ex1', [HomeController::class, 'ex1']);
+
+Route::post('/result1', [HomeController::class, 'result1']);
+
+Route::get('/ex2', [HomeController::class, 'ex2']);
+
+Route::post('/result2', [HomeController::class, 'result2']);
+
+Route::get('/ex3', [HomeController::class, 'ex3']);
+
+Route::post('/result3', [HomeController::class, 'result3']);
+
+Route::get('/ex4', [HomeController::class, 'ex4']);
+
+Route::post('/result4', [HomeController::class, 'result4']);
+
+Route::get('/ex5', [HomeController::class, 'ex5']);
+
+Route::post('/result5', [HomeController::class, 'result5']);
